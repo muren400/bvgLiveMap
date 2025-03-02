339dd5ea915292b8d854bf870ce8249d3a98875b
andi <erzaehlwas@hotmail.de>
2025-03-02 13:41:05 +0100

    fixed yorckstraße and u6

--- src/js/app.js
+++ src/js/app.js
@@ -10,7 +10,7 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const line = urlParams.get('line');
 
-const lines = line != null ? [line.toUpperCase()] : ['U1', 'U2', 'U3', 'U5', 'U6', 'U7', 'U8', 'U9', 'S1', 'S2', 'S3', 'S41', 'S42'];
+const lines = line != null ? [line.toUpperCase()] : null;
 
 if(urlParams.has('showCars')) {
     liveMap.showCars = urlParams.get('showCars').toLocaleLowerCase() === 'true';
